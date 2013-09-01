0077ca78d3c884b8d8403669a9b68024f2e322b9
Josh Vera <vera@github.com>
2013-09-01 15:26:33 -0700

    Remove OCTComment class

--- OctoKit/OCTPullRequestComment.h
+++ OctoKit/OCTPullRequestComment.h
@@ -6,10 +6,31 @@
 //  Copyright (c) 2012 GitHub. All rights reserved.
 //
 
-#import "OCTComment.h"
+#import "OCTObject.h"
 
 // A single comment on a pull request.
-@interface OCTPullRequestComment : OCTComment
+@interface OCTPullRequestComment : OCTObject
+
+// The body of the comment.
+@property (nonatomic, copy, readonly) NSString *body;
+
+// The html body of the comment.
+@property (nonatomic, copy, readonly) NSString *HTMLBody;
+
+// The comment's webpage URL.
+@property (nonatomic, copy, readonly) NSURL *HTMLURL;
+
+// The comment's API URL.
+@property (nonatomic, copy, readonly) NSURL *APIURL;
+
+// The comment's created at date.
+@property (nonatomic, copy, readonly) NSDate *createdAtDate;
+
+// The comment's updated at date.
+@property (nonatomic, copy, readonly) NSDate *updatedAtDate;
+
+// The login of the user who created this comment.
+@property (nonatomic, copy, readonly) NSString *commenterLogin;
 
 // The API URL for the pull request upon which this comment appears.
 @property (nonatomic, copy, readonly) NSURL *pullRequestAPIURL;
