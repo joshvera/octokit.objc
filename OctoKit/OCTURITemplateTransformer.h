ae3ba317ed9956eee58ba02b9cb8b5894c2198b7
Josh Vera <vera@github.com>
2013-08-31 20:44:24 -0700

    Add OCTURITemplateTransformer to project

---
+++ OctoKit/OCTURITemplateTransformer.h
@@ -0,0 +1,16 @@
+//
+//  OCTURITemplateTransformer.h
+//  OctoKit
+//
+//  Created by Josh Vera on 8/31/13.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// The name for a value transformer that converts strings into CSURITemplates and back.
+extern NSString * const OCTURITemplateValueTransformerName;
+
+@interface OCTURITemplateTransformer : NSValueTransformer
+
+@end
