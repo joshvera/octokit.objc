b816dc3d82499792f522707456220262dc68730b
Josh Vera <vera@github.com>
2013-08-31 22:12:01 -0700

    Add URI templates to repo

--- OctoKit/OCTRepository.m
+++ OctoKit/OCTRepository.m
@@ -8,6 +8,9 @@
 
 #import "OCTRepository.h"
 #import "NSValueTransformer+OCTPredefinedTransformerAdditions.h"
+#import "OCTURITemplateTransformer.h"
+#import "OCTUser.h"
+#import "OCTOrganization.h"
 
 static NSString *const OCTRepositoryHTMLIssuesPath = @"issues";
 
@@ -17,31 +20,274 @@ static NSString *const OCTRepositoryHTMLIssuesPath = @"issues";
 
 + (NSDictionary *)JSONKeyPathsByPropertyKey {
 	return [super.JSONKeyPathsByPropertyKey mtl_dictionaryByAddingEntriesFromDictionary:@{
+		@"name": @"name",
+		@"ownerLogin": @"owner.login",
+		@"fullName": @"full_name",
+		@"repoDescription": @"description",
+		@"admin": @"permissions.admin",
+		@"canPush": @"permissions.push",
+		@"canPull": @"permissions.pull",
+		@"private": @"private",
+		@"fork": @"fork",
+		@"owner": @"owner",
+		@"organization": @"organization",
+		@"homepage": @"homepage",
+		@"size": @"size",
+		@"language": @"language",
+		@"hasWiki": @"has_wiki",
+		@"hasIssues": @"has_issues",
+		@"forkCount": @"forks",
+		@"openIssuesCount": @"open_issues",
+		@"watchersCount": @"watchers",
+		@"networkCount": @"network_count",
+		@"defaultBranch": @"default_branch",
+		@"masterBranch": @"master_branch",
+		@"datePushed": @"pushed_at",
+		@"pushedAtDate": @"pushed_at",
+		@"createdAtDate": @"created_at",
+		@"updatedAtDate": @"updated_at",
 		@"HTTPSURL": @"clone_url",
+		@"HTMLURL": @"html_url",
 		@"SSHURL": @"ssh_url",
 		@"gitURL": @"git_url",
-		@"HTMLURL": @"html_url",
-		@"ownerLogin": @"owner.login",
-		@"datePushed": @"pushed_at",
-		@"repoDescription": @"description",
-		@"defaultBranch": @"default_branch",
+		@"SVNURL": @"svn_url",
+		@"APIURITemplate": @"url",
+		@"contributorsURITemplate": @"contributors_url",
+		@"forksURITemplate": @"forks_url",
+		@"mergesURITemplate": @"merges_url",
+		@"notificationsURITemplate": @"notifications_url",
+		@"gitTagsURITemplate": @"git_tags_url",
+		@"gitRefsURITemplate": @"git_refs_url",
+		@"tagsURITemplate": @"tags_url",
+		@"languagesURITemplate": @"languages_url",
+		@"keysURITemplate": @"keys_url",
+		@"hooksURITemplate": @"hooks_url",
+		@"issueEventsURITemplate": @"issue_events_url",
+		@"eventsURITemplate": @"events_url",
+		@"collaboratorsURITemplate": @"collaborators_url",
+		@"teamsURITemplate": @"teams_url",
+		@"assigneesURITemplate": @"assignees_url",
+		@"branchesURITemplate": @"branches_url",
+		@"blobsURITemplate": @"blobs_url",
+		@"stargazersURITemplate": @"stargazers_url",
+		@"subscribersURITemplate": @"subscribers_url",
+		@"commitsURITemplate": @"commits_url",
+		@"issueCommentURITemplate": @"issue_comment_url",
+		@"compareURITemplate": @"compare_url",
+		@"contentsURITemplate": @"contents_url",
+		@"archiveURITemplate": @"archive_url",
+		@"issuesURITemplate": @"issues_url",
+		@"pullsURITemplate": @"pulls_url",
+		@"milestonesURITemplate": @"milestones_url",
+		@"labelsURITemplate": @"labels_url",
+		@"treesURITemplate": @"trees_url",
+		@"statusesURITemplate": @"statuses_url",
+		@"cloneURL": @"clone_url",
+		@"mirrorURL": @"mirror_url",
 	}];
 }
 
-+ (NSValueTransformer *)HTTPSURLJSONTransformer {
++ (NSValueTransformer *)APIURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)adminJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLBooleanValueTransformerName];
+}
++ (NSValueTransformer *)canPushJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLBooleanValueTransformerName];
+}
++ (NSValueTransformer *)canPullJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLBooleanValueTransformerName];
+}
+
++ (NSValueTransformer *)privateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLBooleanValueTransformerName];
+}
+
++ (NSValueTransformer *)forkJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLBooleanValueTransformerName];
+}
+
++ (NSValueTransformer *)ownerJSONTransformer {
+	return [NSValueTransformer mtl_JSONDictionaryTransformerWithModelClass:OCTUser.class];
+}
+
++ (NSValueTransformer *)organizationJSONTransformer {
+	return [NSValueTransformer mtl_JSONDictionaryTransformerWithModelClass:OCTRepository.class];
+}
+
++ (NSValueTransformer *)hasWikiJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLBooleanValueTransformerName];
+}
+
++ (NSValueTransformer *)hasIssuesJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLBooleanValueTransformerName];
+}
+
++ (NSValueTransformer *)datePushedJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTDateValueTransformerName];
+}
+
++ (NSValueTransformer *)pushedAtDateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTDateValueTransformerName];
+}
+
++ (NSValueTransformer *)createdAtDateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTDateValueTransformerName];
+}
+
++ (NSValueTransformer *)updatedAtDateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTDateValueTransformerName];
+}
+
++ (NSValueTransformer *)archiveURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)assigneesURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)blobsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)branchesURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)cloneURLJSONTransformer {
 	return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
 }
 
-+ (NSValueTransformer *)HTMLURLJSONTransformer {
-	return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
++ (NSValueTransformer *)collaboratorsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)commitsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)compareURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)contentsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)contributorsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)eventsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)forksURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
 }
 
 + (NSValueTransformer *)gitURLJSONTransformer {
 	return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
 }
 
-+ (NSValueTransformer *)datePushedJSONTransformer {
-	return [NSValueTransformer valueTransformerForName:OCTDateValueTransformerName];
++ (NSValueTransformer *)gitRefsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)gitTagsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)hooksURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)HTTPSURLJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
++ (NSValueTransformer *)issueCommentURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)issueEventsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)issuesURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)keysURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)labelsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)languagesURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)mergesURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)milestonesURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)mirrorURLJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
++ (NSValueTransformer *)pullsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)stargazersURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)notificationsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)SSHURLJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
++ (NSValueTransformer *)statusesURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)subscribersURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)SVNURLJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
++ (NSValueTransformer *)tagsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
+
++ (NSValueTransformer *)teamsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)treesURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)HTMLURLJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
 }
 
 #pragma mark Derived Properties
