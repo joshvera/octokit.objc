36e17d1c88ce3b4def13fcd2eba562e724bd9a2f
Josh Vera <vera@github.com>
2013-09-01 18:49:54 -0700

    Move OCTSubject stuff back into OCTIssue

--- OctoKit/OCTIssue.h
+++ OctoKit/OCTIssue.h
@@ -9,9 +9,38 @@
 #import "OCTObject.h"
 
 @class OCTPullRequest;
+@class CSURITemplate;
+@class OCTUser;
+
+// The state of the issue. open or closed.
+//
+// OCTIssueStateOpen   - The pull request is open.
+// OCTIssueStateClosed - The pull request is closed.
+typedef enum : NSUInteger {
+    OCTIssueStateOpen,
+    OCTIssueStateClosed
+} OCTIssueState;
 
 // An issue on a repository.
 @interface OCTIssue : OCTObject
+
+@property (nonatomic, assign, readonly) NSUInteger remoteID;
+
+@property (nonatomic, copy, readonly) NSDate *createdAtDate;
+
+@property (nonatomic, copy, readonly) NSDate *updatedAtDate;
+
+@property (nonatomic, copy, readonly) NSString *body;
+
+@property (nonatomic, copy, readonly) NSString *HTMLBody;
+
+@property (nonatomic, copy, readonly) OCTUser *assignee;
+
+@property (nonatomic, copy, readonly) NSString *milestone;
+
+@property (nonatomic, assign, readonly) OCTIssueState state;
+
+@property (nonatomic, copy, readonly) OCTUser *user;
 
 // The webpage URL for this issue.
 @property (nonatomic, copy, readonly) NSURL *HTMLURL;
@@ -22,5 +51,9 @@
 // The pull request that is attached to (i.e., the same as) this issue, or nil
 // if this issue does not have code attached.
 @property (nonatomic, copy, readonly) OCTPullRequest *pullRequest;
+
+@property (nonatomic, copy, readonly) CSURITemplate *APIURITemplate;
+
+@property (nonatomic, copy, readonly) CSURITemplate *commentsURITemplate;
 
 @end
--- OctoKit/OCTIssue.m
+++ OctoKit/OCTIssue.m
@@ -9,6 +9,9 @@
 #import "OCTIssue.h"
 #import "OCTPullRequest.h"
 #import <ReactiveCocoa/EXTKeyPathCoding.h>
+#import "OCTURITemplateTransformer.h"
+#import "NSValueTransformer+OCTPredefinedTransformerAdditions.h"
+#import "OCTUser.h"
 
 @interface OCTIssue ()
 
@@ -38,8 +41,19 @@
 + (NSDictionary *)JSONKeyPathsByPropertyKey {
 	return [super.JSONKeyPathsByPropertyKey mtl_dictionaryByAddingEntriesFromDictionary:@{
 		@"HTMLURL": @"html_url",
-		@"objectID": @"number",
 		@"pullRequestHTMLURL": @"pull_request.html_url",
+		@"commentsURITemplate": @"comments_url",
+		@"HTMLBody": @"body_html",
+		@"user": @"user",
+		@"assignee": @"assignee",
+		@"milestone": @"milestone",
+		@"HTMLURL": @"html_url",
+		@"remoteID": @"number",
+		@"createdAtDate": @"created_at",
+		@"updatedAtDate": @"updated_at",
+		@"APIURITemplate": @"url",
+		@"reviewCommentsURITemplate": @"review_comments_url",
+		@"commentsURITemplate": @"comments_url",
 	}];
 }
 
@@ -49,6 +63,30 @@
 
 + (NSValueTransformer *)pullRequestHTMLURLJSONTransformer {
 	return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
++ (NSValueTransformer *)updatedAtDateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTDateValueTransformerName];
+}
+
++ (NSValueTransformer *)createdAtDateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTDateValueTransformerName];
+}
+
++ (NSValueTransformer *)userJSONTransformer {
+	return [NSValueTransformer mtl_JSONDictionaryTransformerWithModelClass:OCTUser.class];
+}
+
++ (NSValueTransformer *)assigneeJSONTransformer {
+	return [NSValueTransformer mtl_JSONDictionaryTransformerWithModelClass:OCTUser.class];
+}
+
++ (NSValueTransformer *)APIURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)commentsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
 }
 
 @end
