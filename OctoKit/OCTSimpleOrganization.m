674cbab4ebb351fe58133fd56d665d0fe7d3a5d2
Josh Vera <vera@github.com>
2013-11-13 16:44:45 -0500

    Add OCTSimpleOrganization

---
+++ OctoKit/OCTSimpleOrganization.m
@@ -0,0 +1,13 @@
+//
+//  OCTSimpleOrganization.m
+//  OctoKit
+//
+//  Created by Josh Vera on 11/13/13.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import "OCTSimpleOrganization.h"
+
+@implementation OCTSimpleOrganization
+
+@end
