9374bce96a0101bac61e4cc5578ce13260077d8e
Josh Vera <vera@github.com>
2013-11-13 16:46:31 -0500

    Have OCTUser inherit from OCTUserEntity

--- OctoKit/OCTUser.h
+++ OctoKit/OCTUser.h
@@ -6,15 +6,9 @@
 //  Copyright 2010 GitHub. All rights reserved.
 //
 
-#import "OCTOwner.h"
+#import "OCTUserEntity.h"
 
-// A GitHub user.
-@interface OCTUser : OCTOwner
-
-// Returns a user that has the given name and email address.
-+ (instancetype)userWithName:(NSString *)name email:(NSString *)email;
-
-// Returns a user with the given username and OCTServer instance.
-+ (instancetype)userWithLogin:(NSString *)login server:(OCTServer *)server;
+// A public GitHub user.
+@interface OCTUser : OCTUserEntity
 
 @end
--- OctoKit/OCTUser.m
+++ OctoKit/OCTUser.m
@@ -7,37 +7,7 @@
 //
 
 #import "OCTUser.h"
-#import "OCTServer.h"
-#import <ReactiveCocoa/EXTKeyPathCoding.h>
-#import "OCTObject+Private.h"
 
 @implementation OCTUser
-
-#pragma mark Lifecycle
-
-+ (instancetype)userWithName:(NSString *)name email:(NSString *)email {
-	NSMutableDictionary *userDict = [NSMutableDictionary dictionary];
-	if (name != nil) userDict[@keypath(OCTUser.new, name)] = name;
-	if (email != nil) userDict[@keypath(OCTUser.new, email)] = email;
-
-	return [self modelWithDictionary:userDict error:NULL];
-}
-
-+ (instancetype)userWithLogin:(NSString *)login server:(OCTServer *)server {
-	NSMutableDictionary *userDict = [NSMutableDictionary dictionary];
-	if (login != nil) userDict[@keypath(OCTUser.new, login)] = login;
-	if (server.baseURL != nil) userDict[@keypath(OCTUser.new, baseURL)] = server.baseURL;
-
-	return [self modelWithDictionary:userDict error:NULL];
-}
-
-#pragma mark MTLModel
-
-- (void)mergeLoginFromModel:(MTLModel *)model {
-	// Don't ever replace the login property, as this could be different
-	// to the login property returned by the API (eg. LDAP logins
-	// have any characters in [a-z0-9-] replaced with '-' for their GitHub
-	// Enterprise 'login').
-}
 
 @end
