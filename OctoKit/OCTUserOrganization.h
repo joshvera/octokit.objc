691e0eebc31f4fae5046978ce2a7fc82dc20d922
Josh Vera <vera@github.com>
2013-11-13 16:45:30 -0500

    Have OCTUserOrganization inherit from OCTUserEntity

--- OctoKit/OCTUserOrganization.h
+++ OctoKit/OCTUserOrganization.h
@@ -6,31 +6,9 @@
 //  Copyright (c) 2013 GitHub. All rights reserved.
 //
 
-#import "OCTObject.h"
+#import "OCTUserEntity.h"
 
-@class CSURITemplate;
-
-@interface OCTUserOrganization : OCTObject
-
-// The unique name for this organization, used in GitHub URLs.
-@property (nonatomic, copy, readonly) NSString *login;
-
-// The URL for an avatar image.
-@property (nonatomic, copy, readonly) NSURL *avatarURL;
-
-// The organization's API URITemplate.
-@property (nonatomic, copy, readonly) CSURITemplate *APIURITemplate;
-
-// The organization's repos URITemplate.
-@property (nonatomic, copy, readonly) CSURITemplate *reposURITemplate;
-
-// The organization's events URITemplate.
-@property (nonatomic, copy, readonly) CSURITemplate *eventsURITemplate;
-
-// The organization's members URITemplate.
-@property (nonatomic, copy, readonly) CSURITemplate *membersURITemplate;
-
-// The organization's public members URITemplate.
-@property (nonatomic, copy, readonly) CSURITemplate *publicMembersURITemplate;
+// A public user representation of an organization
+@interface OCTUserOrganization : OCTUserEntity
 
 @end
--- OctoKit/OCTUserOrganization.m
+++ OctoKit/OCTUserOrganization.m
@@ -7,45 +7,7 @@
 //
 
 #import "OCTUserOrganization.h"
-#import "OCTURITemplateTransformer.h"
 
 @implementation OCTUserOrganization
-
-#pragma mark MTLJSONSerializing
-
-+ (NSDictionary *)JSONKeyPathsByPropertyKey {
-	return [super.JSONKeyPathsByPropertyKey mtl_dictionaryByAddingEntriesFromDictionary:@{
-		@"APIURITemplate": @"url",
-		@"reposURITemplate": @"repos_url",
-		@"eventsURITemplate": @"events_url",
-		@"membersURITemplate": @"members_url",
-		@"publicMembersURITemplate": @"public_members_url",
-		@"avatarURL": @"avatar_url",
-	}];
-}
-
-+ (NSValueTransformer *)APIURITemplateJSONTransformer {
-	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
-}
-
-+ (NSValueTransformer *)reposURITemplateJSONTransformer {
-	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
-}
-
-+ (NSValueTransformer *)eventsURITemplateJSONTransformer {
-	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
-}
-
-+ (NSValueTransformer *)membersURITemplateJSONTransformer {
-	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
-}
-
-+ (NSValueTransformer *)publicMembersURITemplateJSONTransformer {
-	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
-}
-
-+ (NSValueTransformer *)avatarURLJSONTransformer {
-	return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
-}
 
 @end
