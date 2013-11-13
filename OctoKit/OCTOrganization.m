c88819dd616418314a387ee1cf89bcaf2fd26751
Josh Vera <vera@github.com>
2013-11-13 16:46:49 -0500

    Have OCTOrganization inherit from OCTSimpleOrganization

--- OctoKit/OCTOrganization.m
+++ OctoKit/OCTOrganization.m
@@ -7,19 +7,82 @@
 //
 
 #import "OCTOrganization.h"
-#import "OCTTeam.h"
+#import "OCTURITemplateTransformer.h"
+#import "NSValueTransformer+OCTPredefinedTransformerAdditions.h"
 
 @implementation OCTOrganization
 
-#pragma mark MTLModel
+#pragma mark MTLJSONSerializing
 
-+ (NSValueTransformer *)teamsJSONTransformer {
-	return [NSValueTransformer mtl_JSONArrayTransformerWithModelClass:OCTTeam.class];
++ (NSDictionary *)JSONKeyPathsByPropertyKey {
+	return [[super 
+		JSONKeyPathsByPropertyKey]
+		mtl_dictionaryByAddingEntriesFromDictionary:@{
+			@"login": @"login",
+			@"avatarURL": @"avatar_url",
+			@"APIURITemplate": @"url",
+			@"HTMLURL": @"html_url",
+			@"reposURITemplate": @"repos_url",
+			@"eventsURITemplate": @"events_url",
+			@"receivedEventsURITemplate": @"received_events_url",
+			@"membersURITemplate": @"members_url",
+			@"publicMembersURITemplate": @"public_members_url",
+			@"type": @"type",
+			@"siteAdmin": @"site_admin",
+			@"name": @"name",
+			@"company": @"company",
+			@"blog": @"blog",
+			@"location": @"location",
+			@"email": @"email",
+			@"publicRepoCount": @"public_repos",
+			@"publicGistCount": @"public_gists",
+			@"createdAtDate": @"created_at",
+			@"updatedAtDate": @"updated_at",
+		}];
 }
 
-- (void)mergeTeamsFromModel:(OCTOrganization *)model {
-	// Teams are fetched separately from the actual organization. So when we
-	// merge, the other model's teams will always be nil. Ignore that.
++ (NSValueTransformer *)avatarURLJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
++ (NSValueTransformer *)APIURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)HTMLURLJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
++ (NSValueTransformer *)reposURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)eventsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)receivedEventsURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)membersURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)publicMembersURITemplateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTURITemplateValueTransformerName];
+}
+
++ (NSValueTransformer *)siteAdminJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLBooleanValueTransformerName];
+}
+
++ (NSValueTransformer *)createdAtDateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTDateValueTransformerName];
+}
+
++ (NSValueTransformer *)updatedAtDateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTDateValueTransformerName];
 }
 
 @end
