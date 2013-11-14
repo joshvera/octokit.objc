3efb5eac09380173494c06fa5690fca243e9bb0f
Josh Vera <vera@github.com>
2013-11-13 20:36:47 -0500

    Make OCTUserOrganization conform to MTLJSONSerializing

--- OctoKit/OCTUserOrganization.h
+++ OctoKit/OCTUserOrganization.h
@@ -6,9 +6,36 @@
 //  Copyright (c) 2013 GitHub. All rights reserved.
 //
 
-#import "OCTUserEntity.h"
+#import "OCTOrganizationEntity.h"
+#import "OCTUserInfo.h"
 
 // A public user representation of an organization
-@interface OCTUserOrganization : OCTUserEntity
+@interface OCTUserOrganization : OCTOrganizationEntity <OCTUserInfo>
+
+@property (nonatomic, copy, readonly) NSString *name;
+
+@property (nonatomic, copy, readonly) NSString *company;
+
+@property (nonatomic, copy, readonly) NSString *blog;
+
+@property (nonatomic, copy, readonly) NSString *location;
+
+@property (nonatomic, copy, readonly) NSString *email;
+
+@property (nonatomic, assign, readonly, getter = isHireable) BOOL hireable;
+
+@property (nonatomic, copy, readonly) NSString *bio;
+
+@property (nonatomic, assign, readonly) NSUInteger publicRepoCount;
+
+@property (nonatomic, assign, readonly) NSUInteger followerCount;
+
+@property (nonatomic, assign, readonly) NSUInteger followingCount;
+
+@property (nonatomic, copy, readonly) NSDate *createdAtDate;
+
+@property (nonatomic, copy, readonly) NSDate *updatedAtDate;
+
+@property (nonatomic, assign, readonly) NSUInteger publicGistCount;
 
 @end
