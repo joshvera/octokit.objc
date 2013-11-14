4e7e70635701fe5abe213550879c6da5d3d29563
Josh Vera <vera@github.com>
2013-11-13 20:32:59 -0500

    Have OCTUser inherit from OCTUserEntity and conform to OCTUserInfo

--- OctoKit/OCTUser.m
+++ OctoKit/OCTUser.m
@@ -7,7 +7,41 @@
 //
 
 #import "OCTUser.h"
+#import "OCTURITemplateTransformer.h"
+#import "NSValueTransformer+OCTPredefinedTransformerAdditions.h"
 
 @implementation OCTUser
+
++ (NSDictionary *)JSONKeyPathsByPropertyKey {
+	return [[super
+		JSONKeyPathsByPropertyKey]
+		mtl_dictionaryByAddingEntriesFromDictionary:@{
+			@"name": @"name",
+			@"company": @"company",
+			@"blog": @"blog",
+			@"location": @"location",
+			@"email": @"email",
+			@"hireable": @"hireable",
+			@"bio": @"bio",
+			@"publicRepoCount": @"public_repos",
+			@"followerCount": @"followers",
+			@"followingCount": @"following",
+			@"createdAtDate": @"created_at",
+			@"updatedAtDate": @"updated_at",
+			@"publicGistCount": @"public_gists",
+		}];
+}
+
++ (NSValueTransformer *)hireableJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:MTLBooleanValueTransformerName];
+}
+
++ (NSValueTransformer *)createdAtDateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTDateValueTransformerName];
+}
+
++ (NSValueTransformer *)updatedAtDateJSONTransformer {
+	return [NSValueTransformer valueTransformerForName:OCTDateValueTransformerName];
+}
 
 @end
