421e05938e310deede1ce4da8df49d96bcd7ac43
Josh Vera <vera@github.com>
2013-11-13 20:36:23 -0500

    Remove properties from OCTUserEntity

--- OctoKit/OCTUserEntity.m
+++ OctoKit/OCTUserEntity.m
@@ -7,39 +7,7 @@
 //
 
 #import "OCTUserEntity.h"
-#import "OCTURITemplateTransformer.h"
-#import "NSValueTransformer+OCTPredefinedTransformerAdditions.h"
 
 @implementation OCTUserEntity
-
-#pragma mark MTLJSONSerializing
-
-+ (NSDictionary *)JSONKeyPathsByPropertyKey {
-	return [[super 
-		JSONKeyPathsByPropertyKey]
-		mtl_dictionaryByAddingEntriesFromDictionary:@{
-			@"name": @"name",
-			@"company": @"company",
-			@"blog": @"blog",
-			@"location": @"location",
-			@"email": @"email",
-			@"hireable": @"hireable",
-			@"bio": @"bio",
-			@"publicRepoCount": @"public_repos",
-			@"followerCount": @"followers",
-			@"followingCount": @"following",
-			@"createdAtDate": @"created_at",
-			@"updatedAtDate": @"updated_at",
-			@"publicGistCount": @"public_gists",
-		}];
-}
-
-+ (NSValueTransformer *)createdAtDateJSONTransformer {
-	return [NSValueTransformer valueTransformerForName:OCTDateValueTransformerName];
-}
-
-+ (NSValueTransformer *)updatedAtDateJSONTransformer {
-	return [NSValueTransformer valueTransformerForName:OCTDateValueTransformerName];
-}
 
 @end
