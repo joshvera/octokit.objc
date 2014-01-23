f640137daa28ac8b4501a91c7e12bcaf05f5672a
Josh Vera <vera@github.com>
2014-01-23 17:32:18 -0500

    Add OCTOrganizationOwner

---
+++ OctoKit/OCTOrganizationOwner.h
@@ -0,0 +1,13 @@
+//
+//  OCTOrganizationOwner
+//  OctoKit
+//
+//  Created by Josh Vera on 11/13/13.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import "OCTEntity.h"
+
+@interface OCTOrganizationOwner : OCTEntity
+
+@end
---
+++ OctoKit/OCTOrganizationOwner.m
@@ -0,0 +1,13 @@
+//
+//  OCTOrganizationOwner.m
+//  OctoKit
+//
+//  Created by Josh Vera on 11/13/13.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import "OCTOrganizationOwner.h"
+
+@implementation OCTOrganizationOwner
+
+@end
